0ba65f6ff048eec808ae1bb07a4b9cc71db410c5
ksdevinney <katie@devinney.org>
2022-04-29 15:36:21 -0500

    personalized name

--- website/js/script.js
+++ website/js/script.js
@@ -3,3 +3,31 @@ function changeStyle(style) {
 
     stylesheet.setAttribute('href', style)
 }
+
+// Ask the user for a name to personalize the page. Display their name prominently on the page.
+function nameOnPage() {
+    console.log("this function loaded")
+    const name = prompt("Enter your name.")
+    
+    if (!name) {
+        document.getElementById("welcome").innerHTML = "Welcome!"
+    } else {
+        document.getElementById("welcome").innerHTML = `Hello, ${name}!`
+    }
+}
+
+// window.addEventListener(onload, nameOnPage)
+
+// Create a Responsive Top navbar
+// Adapt the existing nav menu to collapse on small screens.
+// Show 'hamburger' image for small screen nav select button.
+// Add the new menu to all site pages.
+
+function navToggle() {
+    let bar = document.getElementById("myTopnav");
+    if (bar.className === "topnav") {
+      bar.className += " responsive";
+    } else {
+      bar.className = "topnav";
+    }
+  }
